65b073b05100ad3505fbd860c4c8ffbfc058b573
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2021-02-01 22:35:23 +0900

    Taskコントローラに雑にエンドポイントを定義する

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  @Get()
+  getTasks() {
+    return 'getTasks Success!';
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id', ParseIntPipe) id: number) {
+    return `getTaskById Success! Parameter [id:${id}]`;
+  }
+
+  @Post()
+  createTask(@Body('title') title: string, @Body('description') description: string) {
+    return `createTask Success! Parameter [title:${title}, description:${description}]`;
+  }
+
+  @Patch('/:id')
+  updateTask(@Param('id', ParseIntPipe) id: number, @Body('status') status: string) {
+    return `updateTask Success! Parameter [id:${id}, status:${status}]`;
+  }
+}
