a28fca324548cdc36dfccbe1f7432f8e82cc22dd
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2021-02-03 23:14:11 +0900

    DBセットアップと空のサービス作成

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      database: 'nest_todo',
+      synchronize: true,
+      entities: [__dirname + '/**/*.entity.{js,ts}'],
+    }),
+  ],
 })
 export class AppModule {}
